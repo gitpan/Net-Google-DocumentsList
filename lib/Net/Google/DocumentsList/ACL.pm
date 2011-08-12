66f01e628a706068e65e4ee6b9265761e1dcf5bb
Nobuo Danjou <danjou@soffritto.org>
2011-08-12 02:40:45 +0000

    Import of DANJOU/Net-Google-DocumentsList-0.06 from CPAN.
    gitpan-cpan-distribution: Net-Google-DocumentsList
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         DANJOU/Net-Google-DocumentsList-0.06.tar.gz
    gitpan-cpan-author:       DANJOU
    gitpan-cpan-maturity:     released

--- lib/Net/Google/DocumentsList/ACL.pm
+++ lib/Net/Google/DocumentsList/ACL.pm
@@ -1,7 +1,7 @@
 package Net::Google::DocumentsList::ACL;
 use Any::Moose;
 use Net::Google::DataAPI;
-use XML::Atom::Util qw(first);
+use XML::Atom::Util qw(first create_element);
 use Net::Google::DocumentsList::Types;
 with 'Net::Google::DocumentsList::Role::EntryWithoutEtag';
 
@@ -14,7 +14,16 @@ entry_has 'role' => (
     },
     to_atom => sub {
         my ($self, $atom) = @_;
-        $atom->set($self->ns('gAcl'),'role', '', {value => $self->role});
+        if (my $key = $self->withKey) {
+            my $role = create_element($self->ns('gAcl')->{uri}, 'role');
+            $role->setAttribute(value => $self->role);
+            my $withKey = create_element($self->ns('gAcl')->{uri}, 'withKey');
+            $withKey->setAttribute(key => $key);
+            $withKey->appendChild($role);
+            $atom->set($self->ns('gAcl')->{uri}, 'withKey', $withKey);
+        } else {
+            $atom->set($self->ns('gAcl'),'role', '', {value => $self->role});
+        }
     }
 );
 entry_has 'scope' => (
@@ -23,14 +32,26 @@ entry_has 'scope' => (
     from_atom => sub {
         my ($self, $atom) = @_;
         my $elem = first($atom->elem, $self->ns('gAcl')->{uri}, 'scope');
-        return {
-            value => $elem->getAttribute('value'),
+        my $obj = {
             type  => $elem->getAttribute('type'),
         };
+        if (my $value = $elem->getAttribute('value')) {
+            $obj->{value} = $value;
+        }
+        return $obj;
     },
     to_atom => sub {
         my ($self, $atom) = @_;
         $atom->set($self->ns('gAcl'),'scope', '', $self->scope);
+    },
+);
+entry_has 'withKey' => (
+    is => 'ro',
+    from_atom => sub {
+        my ($self, $atom) = @_;
+        if (my $elem = first($atom->elem, $self->ns('gAcl')->{uri}, 'withKey')) {
+            $elem->getAttribute('key')
+        }
     },
 );
 
@@ -96,12 +117,28 @@ Net::Google::DocumentsList::ACL - Access Control List object for Google Document
 
   # adding acl
   $doc->add_acl(
-    role => 'reader',
-    scope => {
-        type => 'user',
-        value => 'foo.bar@gmail.com',
+    {
+        role => 'reader',
+        scope => {
+            type => 'user',
+            value => 'foo.bar@gmail.com',
+        }
     }
   );
+
+  # adding acl with authorization keys
+  # users who knows the key can write the doc
+  my $acl = $doc->add_acl(
+    {
+        role => 'writer',
+        scope => {
+            type => 'default',
+        },
+        withKey => 1,
+    }
+  );
+  say $acl->withKey; # this will show the key, and google (not you) makes the key
+
 
 =head1 DESCRIPTION
 
@@ -114,10 +151,12 @@ This module represents Access Control List object for Google Documents List Data
 adds new ACL to document or folder.
 
   $doc->add_acl(
-    role => 'reader',
-    scope => {
-        type => 'user',
-        value => 'foo.bar@gmail.com',
+    {
+        role => 'reader',
+        scope => {
+            type => 'user',
+            value => 'foo.bar@gmail.com',
+        }
     }
   );
 
